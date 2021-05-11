f0fb07848e67873adc7938204182703d419ca63b
ahirsch <Esyon4711#>
2021-05-11 21:52:05 +0200

    removed dependencies

--- src/routes/api/cv.js
+++ src/routes/api/cv.js
@@ -1,22 +1,7 @@
-import CVContent from '$lib/cv.yaml';
-
 export async function get() {
-	
-	try {
-		return {
-			body: {
-				success: true,
-				cv: CVContent
-			}
-		};
-	} catch (e) {
-		console.log(e);
-	}
-
 	return {
 		body: {
-			success: false,
-			cv: ''
+			success: true,
 		}
 	};
 }
--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,8 @@
-import yaml from '@rollup/plugin-yaml';
-import preprocess from 'svelte-preprocess';
-import vercel from '@sveltejs/adapter-vercel';
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: preprocess({}),
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		
-		target: '#svelte',
-		adapter: vercel(),
-		vite: () => ({
-			plugins: [yaml()],
-			
-		})
+		target: '#svelte'
 	}
 };
 
