d76ef712b084e52d72aecac2883f4b15e545e279
BetaConnector <ahirsch29@gmail.com>
2021-05-11 21:26:39 +0200

    Update index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,7 +2,7 @@
 <script>
 	import { onMount } from 'svelte';
 	onMount(async () => {
-		await fetch('/api/cv.json').then((r) => r.json()); // workin
+		await fetch('/api/cv.json').then((r) => r.json()); // working
 		await fetch('/api/cv').then((r) => r.json()); // not working
 	});
 
